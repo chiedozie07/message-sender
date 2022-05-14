6c8f54294a10e5a1efb088ccd17de7985b210c1c
your_username <ezidiegwuchiedozie17@gmail.com>
2022-05-14 22:54:33 +0200

    added new folders

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(`${accountSid}`, `${authToken}`);
 
-//server
+//DZ SMS server
 const app = express();
 const port = process.env.PORT || 5000;
 
